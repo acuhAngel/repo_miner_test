b7d6be3a1b2b3df1002bfb11624f90d82c11abb7
OswaldoFalcon <owa11@hotmail.com>
2022-05-27 13:44:24 -0500

    add import_config_core

--- config/config.exs
+++ config/config.exs
@@ -20,6 +20,7 @@ config :amqp,
   ]
 
 import_config "../apps/repo_miner_web/config/config.exs"
+import_config "../apps/repo_miner_core/config/config.exs"
 # import_config "../apps/repo_miner_web/config/config.exs"
 config :repo_miner_analysis, :pool_size, 10
 
