4257fd9d249c055c99dbc0a4c1b8f0ef11bda066
OswaldoFalcon <owa11@hotmail.com>
2022-05-30 12:36:21 -0500

    CRUD User_commits

--- .iex.exs
+++ .iex.exs
@@ -4,4 +4,9 @@ alias RepoMinerCore.CodeRepoService.Repository
 alias RepoMinerCore.CodeRepoService.Status
 alias RepoMinerCore.CodeRepoService.CommitsDensity
 alias RepoMinerCore.CodeRepoService.Branches
+alias RepoMinerCore.CodeRepoService.UserCommits
+alias RepoMinerCore.CodeRepoService.UserCommitsService
+alias RepoMinerCore.CodeRepoService.RepositoryService
+alias RepoMinerCore.CodeRepoService.CommitsDensityService
+
 
